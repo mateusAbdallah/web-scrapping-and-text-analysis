b8f32e78c436009b425a090f4f07abdce9852f21
Mateus Abdallah <abdallahmateus@gmail.com>
2025-03-14 22:38:17 -0700

    create csv file to write title and first paragraph from each url

--- part01.py
+++ part01.py
@@ -13,7 +13,7 @@ soup = BeautifulSoup(html, 'html.parser')
 
 # %%
 
-paragraphs = soup.find_all('p')
+# paragraphs = soup.find_all('p')
 # links = [for a in paragraphs]
 
 # %%
@@ -21,3 +21,10 @@ paragraphs = soup.find_all('p')
 links = [a['href'] for a in soup.find_all('a', href=True) if a['href'].startswith('/wiki/')]
 unique_a = set(links)
 list_unique_a = list(unique_a)
+
+# %%
+
+with open("wiki_paragraphs.csv", "w", newline="", encoding="utf-8") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['title', 'first_paragraph'])
+
