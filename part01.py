3220f8cbae03c7fc1a5a0c7e1df6d7bbe125b63e
Mateus Abdallah <abdallahmateus@gmail.com>
2025-03-14 20:38:20 -0700

    get all hrefs from a's tags

--- part01.py
+++ part01.py
@@ -5,7 +5,19 @@ import requests
 import csv
  
 # %%
-URL = 'https://en.wikipedia.org/wiki/Main_Page'
-response = requests.get(URL)
 
-response.status_code
+URL = 'https://en.wikipedia.org'
+response = requests.get(URL)
+html = response.content
+soup = BeautifulSoup(html, 'html.parser')
+
+# %%
+
+paragraphs = soup.find_all('p')
+# links = [for a in paragraphs]
+
+# %%
+
+links = [a['href'] for a in soup.find_all('a', href=True) if a['href'].startswith('/wiki/')]
+unique_a = set(links)
+list_unique_a = list(unique_a)
