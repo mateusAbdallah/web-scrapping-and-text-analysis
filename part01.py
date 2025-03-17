f083b1aa4e3d17a291fc10bc6cbb9ec7d3560bcc
Mateus Abdallah <abdallahmateus@gmail.com>
2025-03-16 20:37:13 -0700

    writing contents in a csv file

--- part01.py
+++ part01.py
@@ -3,11 +3,12 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import time
  
 # %%
 
-URL = 'https://en.wikipedia.org'
-response = requests.get(URL)
+BASE_URL = 'https://en.wikipedia.org'
+response = requests.get(BASE_URL)
 html = response.content
 soup = BeautifulSoup(html, 'html.parser')
 
@@ -28,3 +29,18 @@ with open("wiki_paragraphs.csv", "w", newline="", encoding="utf-8") as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['title', 'first_paragraph'])
 
+    for link in list_unique_a[:2]:
+        page_url = BASE_URL + link
+        page_response = requests.get(page_url)
+
+        if page_response.status_code == 200:
+            page_soup = BeautifulSoup(page_response.content, 'html.parser')
+            
+            first_paragraph_tag = page_soup.find('p')
+            clean_paragraph = first_paragraph_tag.text.strip()
+            page_title = page_soup.title.string
+
+            writer.writerow([page_title, clean_paragraph])
+        
+        else:
+            print(f'Failed to get {page_url}')
